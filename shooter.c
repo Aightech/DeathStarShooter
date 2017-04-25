a83f32f56710e02e8006cf3df3556f711cd7e44c
HANNA Elias <3408902@pc4046.polytech.upmc.fr>
2017-04-25 18:01:09 +0200

    début de seuillage avec HSV

--- shooter.c
+++ shooter.c
@@ -1,30 +1,70 @@
-#include <opencv/cv.h>
 #include <opencv/highgui.h>
+#include <opencv/cv.h>
 #include <stdio.h>
 
 int main()
 {
+  int i;
+  int cpt;
+  int sumx;
+  int sumy;
+  int cols;
+  int rows;
+  char *edgesData;
   CvCapture *cap;
+  IplImage *edges;
+  IplImage *edges_HSV;
+  IplImage *edges_threshed;
+  IplImage *frame; 
+  CvPoint centre;
+  CvScalar color;
+  color.val[1] = 0;
+  color.val[2] = 255;
+  color.val[3] = 0;
   cap=cvCreateCameraCapture(0);
   if(!cap)
     {
-    printf("Erreur lors de l'ouverture de la caméra");
-  }
+      printf("Erreur lors de l'ouverture de la caméra");
+    }
   else
     {
       cvNamedWindow("Window", CV_WINDOW_AUTOSIZE);
-      for(;;)
+      while(1)
 	{
-	  IplImage *frame = cvQueryFrame(cap);
+	  frame = cvQueryFrame(cap);
 	  if(!frame)
 	    {
 	      printf("Erreur lors de l'acquisition de l'image");
 	      break;
 	    }
+	  edges_HSV = cvCreateImage(cvGetSize(frame), 8, 3);
+	  cvCvtColor(frame, edges_HSV, CV_BGR2HSV);
+	  edges_threshed = cvCreateImage(cvGetSize(frame), 8, 1);
+	  cvInRangeS(edges_HSV, cvScalar(20,100,100), cvScalar(30,255,255),edges_threshed);
+	  cvReleaseImage(&edges_HSV);
+	  /*cvCvtColor(frame, edges, CV_RGB2GRAY);
+	    cvThreshold(edges, edges, 80, 255,0);
+	    cpt = 0;
+	    sumx= 0;
+	    sumy= 0;
+	  cols = edges->width;
+	  rows = edges->height;
+	  edgesData = edges->imageData;
+	  for(i = 0 ; i < rows*cols ; i++)
+	    {
+	      if(edgesData[i] == 0)
+		{
+		  sumy += i/cols;
+		  sumx += i%cols;
+		  cpt++;
+		}
+	    }
+	  centre = cvPoint(sumx/cpt, sumy/cpt);
+	  cvCircle(frame, centre, 30, color, -1, 8, 0);*/
 	  cvShowImage("Window",frame);
 	  cvWaitKey(27);
 	}
-      cvCamShift(,"Window",);
+      
       cvReleaseCapture(&cap);
       cvDestroyWindow("Window");
     }
