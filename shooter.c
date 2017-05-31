7cac0607828c54f72d83c0015c785cf6c6de9461
Elias Hanna <elias.hanna@etu.upmc.fr>
2017-05-31 10:35:16 +0200

    version finale avant merge mais bien finale

--- shooter.c
+++ shooter.c
@@ -13,23 +13,20 @@
 
 
 
-
 int main()
 {
-	cJoystick js;
+  cJoystick js;
        
-       GUI gui;
-       initGUI(&gui);
-       createMenu(&gui);
-	//Pantilt pantilt = {80,80,40,140,40,100,0.3};
-	//initPantilt();
+  GUI gui;
+  initGUI(&gui);
+  createMenu(&gui);
+  //Pantilt pantilt = {80,80,40,140,40,100,0.3};
+  //initPantilt();
 
   Camera cam;
   struct _CGI afterEffect;
   Patatoide patate;
-  int h_resized, w_resized;
-  CvRect ROI;
-  int frameNumber = 0;
+  cam.frameNumber = 0;
 
   init_mask(&afterEffect);
   patate.moy = 1500;
@@ -45,77 +42,132 @@ int main()
 	{
 	  printf("Erreur lors de l'ouverture de l'étoile de la mort !! Mais que fait Anakin ??\n");
 	}
-      afterEffect.Cockpit = init_cockpit();/* Initialisations liées à l'image du cockpit */
+      
+      afterEffect.Explosion = cvLoadImage("explosion.jpg", CV_LOAD_IMAGE_COLOR); /* On charge l'image de l'étoile de la mort */
 
       /*Création du mask permettant d'enlever le fond du cockpit*/
 
-      create_mask(&afterEffect, 0, 0, 0);
 
       while(1)
 	{
-	  frameNumber++;
-	  if(frameNumber == 10)
-	    frameNumber = 0;
-	  cam.frame = cvQueryFrame(cam.cap);
-	  if(!cam.frame)
+	  Event event;
+	  while(gui.window.pollEvent(event))
 	    {
-	      printf("Erreur lors de l'acquisition de l'image\n");
+	      if(event.type == Event::Closed)
+		gui.window.close();
+	    }
+              
+	  switch(gui.display)
+	    {
+	    case 0:
+	      {
+		gui.selected+=(js.joystickValue(1)>10000)?0.0015:0;
+		gui.selected+=(js.joystickValue(1)<-10000)?-0.0015:0;
+		if(js.buttonPressed(0)>0)
+		  {
+		    switch((int)gui.selected%3)
+		      {
+		      case 0:
+			gui.display=1;
+			createMenu(&gui);
+			break;
+                                          
+		      case 1:
+			break;
+                                          
+		      case 2:
+			gui.window.close();
+			break;
+		      }
+		  }
+		updateGUI(&gui);
+	      }
+	      break;
+	    case 1:
+	      {
+		gui.selected+=(js.joystickValue(0)>10000)?0.0015:0;
+		gui.selected+=(js.joystickValue(0)<-10000)?-0.0015:0;
+		if(js.buttonPressed(0)>0)
+		  {
+		    gui.starship=(int)gui.selected%3;
+		    afterEffect.Cockpit = init_cockpit(gui.starship);/* Initialisations liées à l'image du cockpit */
+		    create_mask(&afterEffect, 0, 0, 0);
+		    gui.display=2;
+		    //createMenu(&gui);
+		  }
+		updateGUI(&gui);
+	      }
+	      break;
+	    case 2:
+	      {
+                           
+		gui.window.clear(Color(48,48,48));
+		Texture texture;
+                            
+		texture.create(W, H); 
+                            
+		unsigned char pixels[W*H*4];
+
+		for(int i = 0; i < W*H*4; i += 4) {
+		  pixels[i] = 0; // obviously, assign the values you need here to form your color
+		  pixels[i+1] = 0;
+		  pixels[i+2] = 0;
+		  pixels[i+3] = 255;
+		}
+
+		texture.update(pixels);
+		Sprite sprite;
+		sprite.setTexture(texture);// Setting the texture for the sprites
+		sprite.setPosition(Vector2f(0,0));
+                            
+		gui.window.draw(sprite);
+		gui.window.display();//updateGUI(&gui);
+
+		cam.frameNumber++;
+		if(cam.frameNumber == 10)
+		  cam.frameNumber = 0;
+		cam.frame = cvQueryFrame(cam.cap);
+		if(!cam.frame)
+		  {
+		    printf("Erreur lors de l'acquisition de l'image\n");
+		    break;
+		  }
+
+		/*On créé nos images que l'on va utiliser*/
+
+		cam.HSV = cvCreateImage(cvGetSize(cam.frame), 8, 3);
+		cam.threshed = cvCreateImage(cvGetSize(cam.frame), 8, 1);
+
+		/*On passe de l'espace BGR à l'espace HSV. On stocke l'image dans edges_HSV*/
+
+		cvCvtColor(cam.frame, cam.HSV, CV_BGR2HSV);
+
+		/*On binarise l'image edges_HSV en la seuillant avec des bornes basses et hautes (pour garder uniquement le bleu)*/
+
+		cvInRangeS(cam.HSV, cam.lowerBound, cam.higherBound, cam.threshed);
+
+		/*EROSION*/
+
+		cvErode(cam.threshed, cam.threshed, cam.elem, 1);
+
+		/*Calcul du centre de l'image et du nombre moyen de pixels détectés faire focntion qui retourne struc centre*/
+
+		calcul_patate(&patate, &cam, 0.1);
+
+		insert_image(&afterEffect, &cam, &patate, 1);/* On insère l'étoile de la mort */
+		insert_image(&afterEffect, &cam, &patate, 2);/* On insère l'explosion */
+		insert_image(&afterEffect, &cam, &patate, 0);/* On insère le cockpit */
+
+		laser(cam.frame, cam.frameNumber);
+
+		release_boucle(&afterEffect, &cam);
+		if(cvWaitKey(27) != -1)
+		  break;
+	      }
 	      break;
 	    }
-
-	  /*On créé nos images que l'on va utiliser*/
-
-	  cam.HSV = cvCreateImage(cvGetSize(cam.frame), 8, 3);
-	  cam.threshed = cvCreateImage(cvGetSize(cam.frame), 8, 1);
-
-	  /*On passe de l'espace BGR à l'espace HSV. On stocke l'image dans edges_HSV*/
-
-	  cvCvtColor(cam.frame, cam.HSV, CV_BGR2HSV);
-
-	  /*On binarise l'image edges_HSV en la seuillant avec des bornes basses et hautes (pour garder uniquement le bleu)*/
-
-	  cvInRangeS(cam.HSV, cam.lowerBound, cam.higherBound, cam.threshed);
-
-	  /*EROSION*/
-
-	  cvErode(cam.threshed, cam.threshed, cam.elem, 1);
-
-	  /*Calcul du centre de l'image et du nombre moyen de pixels détectés faire focntion qui retourne struc centre*/
-
-	  calcul_patate(&patate, &cam, 0.1);
-
-	  /*Image ROI*/
-
-	  h_resized = (int)( (afterEffect.DeathStar->height*patate.percentage)/100 );
-	  w_resized = (int)( (afterEffect.DeathStar->width*patate.percentage)/100 );
-
-	  afterEffect.DeathStar_resized = resize(afterEffect.DeathStar, patate.percentage);
-
-	  /*Création du mask permettant d'enlever le fond de l'étoile de la mort*/
-
-	  create_mask(&afterEffect, 1, w_resized, h_resized);
-
-	  /* ROI ROI ROI */
-	  ROI = cvRect(patate.centre.x - (int)(w_resized/2), patate.centre.y - (int)(h_resized/2), w_resized, h_resized);
-	  cvSetImageROI(cam.frame, ROI);                                /*on set le ROI de l'image frame*/
-	  if(cam.frame->roi->height == h_resized && cam.frame->roi->width == w_resized)
-	    cvCopy(afterEffect.DeathStar_resized, cam.frame, afterEffect.mask_DeathStar);/*on copie l'étoile de la mort sur l'image (dans la ROI)*/
-
-	  cvResetImageROI(cam.frame);
-
-	  laser(cam.frame, frameNumber);
-
-	  /* Copie du cockpit sur l'image principale*/
-	  ROI = cvRect(0,0,afterEffect.Cockpit->width,afterEffect.Cockpit->height);
-	  cvSetImageROI(cam.frame,ROI);
-	  if(afterEffect.Cockpit->height == cam.frame->roi->height && afterEffect.Cockpit->width == cam.frame->roi->width)
-	    cvCopy(afterEffect.Cockpit, cam.frame, afterEffect.mask_Cockpit);
-	  else
-	    printf("Erreur pas la même taille sur la copie du cockpit\n");
-
-	  release_boucle(&afterEffect, &cam);
-	  if(cvWaitKey(27) != -1)
-	    break;
+              
+	  
 	}
       release_final(&cam, &afterEffect);
     }
